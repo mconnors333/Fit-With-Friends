39c54a176c7c6f0d6b92dddd78c5c534d6f3cf6e
mconnors333 <matthew.connors333@gmail.com>
2017-03-08 14:03:32 -0500

    Initial Commit

---
+++ Fit with Friends/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Fit with Friends
+//
+//  Created by Matthew Connors on 3/8/17.
+//  Copyright © 2017 Matt Connors. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
